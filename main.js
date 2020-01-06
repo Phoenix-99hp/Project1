7ed41bdec3b8b6219ad81330f3d79e83964ddfc5
Phoenix <drake.mcgarrah@gmail.com>
2020-01-06 14:00:56 -0600

    Set up city variable for later use

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=WJBXz9pjG5TmRa7XUYBxAoBwsZnR4TZT";
-
+var city = "Austin";
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?" + "city=" + city + "&apikey=WJBXz9pjG5TmRa7XUYBxAoBwsZnR4TZT";
 $.ajax({
     url: queryURL,
     method: "GET"
