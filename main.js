e52d0a48e1ecc3848a6d7b2d9cc61c304c99da68
Phoenix <drake.mcgarrah@gmail.com>
2020-01-10 12:54:51 -0600

    Deleted console.logs and comments

--- main.js
+++ main.js
@@ -7,18 +7,9 @@ $("#city-search").on("click", function (event) {
     event.preventDefault();
     $(".event-content").empty();
     var city = $("#city").val().trim();
-    // var startMonthQuery = $("#startDate").val().trim().slice(0, 2);
-    // var startDayQuery = $("#startDate").val().trim().slice(3, 5);
-    // var startYearQuery = $("#startDate").val().trim().slice(6);
-    // var endMonthQuery = $("#endDate").val().trim().slice(0, 2);
-    // var endDayQuery = $("#endDate").val().trim().slice(3, 5);
-    // var endYearQuery = $("#endDate").val().trim().slice(6);
 
     var startDateQuery = $("#startDate").val().trim() + "T00:00:00Z";
     var endDateQuery = $("#endDate").val().trim() + "T23:59:59Z";
-    console.log($("#startDate").val().trim());
-    // var startDateQuery = (startYearQuery + "-" + startMonthQuery + "-" + startDayQuery + "T00:00:00Z");
-    // var endDateQuery = (endYearQuery + "-" + endMonthQuery + "-" + endDayQuery + "T23:59:59Z");
 
     var queryURL = "https://app.ticketmaster.com/discovery/v2/events?apikey=WJBXz9pjG5TmRa7XUYBxAoBwsZnR4TZT&locale=*" + "&startDateTime=" + startDateQuery + "&endDateTime=" + endDateQuery + "&city=" + city + "&size=199" + "&sort=date,asc";
     $.ajax({
@@ -139,7 +130,6 @@ $("#city-search").on("click", function (event) {
 
 $("#more").on("click", function (event) {
     event.preventDefault();
-    console.log("button was clicked!");
     var lastEventId = [""];
     lastEventId.splice(0, 1, $("#storage").text());
     $(".event-content").empty();
@@ -157,7 +147,6 @@ $("#more").on("click", function (event) {
         function checkForLastEvent() {
             for (i = 0; i < response._embedded.events.length; i++)
                 if (response._embedded.events[i].id == lastEventId[0]) {
-                    console.log(i);
                     return i;
                 }
         }
@@ -165,7 +154,6 @@ $("#more").on("click", function (event) {
         var lastEventIndex = checkForLastEvent();
 
         function generateNextFiveEvents() {
-            console.log("generating next five...");
             for (i = lastEventIndex; i < response._embedded.events.length; i++) {
                 if (response._embedded.events[i].name == "No Longer on Sale for Web") { continue; }
                 else if ($(".listing").length < 5) {
